391968c61d045bf5578b6c4f18e740330237344c
MohdAnas07 <anas98144@gmail.com>
2022-04-01 23:15:01 +0530

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import Body from './components/body';
-// import Particles from 'react-particles-js';
 function App() {
   return (
     <>
-
       <Header />
       <Body />
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
   return (
     <div className="header">
 
+
       <h2 className="college-name">Bipin Tripathi Kumaon Institute of Technology</h2>
       <div className="header-container">
         <div className="left-container">
@@ -20,7 +21,6 @@ export default function Header() {
             <h1 className="fest-h3">2K22</h1>
           </div>
           <p>Fest Start from 18 April to 19 April 2K22</p>
-
         </div>
 
       </div>
