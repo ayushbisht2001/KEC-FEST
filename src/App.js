5a7468e873f193c4407eb2a9f96b79dd0d977a23
Ayush Bisht <bisht.ayush2001@gmail.com>
2022-04-01 19:56:38 +0530

    project shift

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import Body from './components/body';
+function App() {
+  return (
+  <>
+  <Header />
+  <Body />
+
+</>
+  );
+}
+
+export default App;
