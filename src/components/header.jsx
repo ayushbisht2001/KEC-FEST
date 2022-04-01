204fba3daf7a5bf83521226d00f85f8a06b19f67
MohdAnas07 <anas98144@gmail.com>
2022-04-01 22:29:04 +0530

    front-page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import "./style/header.css";
+import logo from '../kvlogo.png'
+// import Particles from 'react-particles-js';
 
 export default function Header() {
   return (
-    <div className="container-fluid  bg-dark ">
-      <div className="container header p-4">
-        <h2 className="heading ">
-          {" "}
-          Bipin Tripathi Kuamon Institute of Techonology
-        </h2>
+    <div className="header">
 
-        <h1 className="heading mt-4" >Kaivalya</h1>
-        <div className="logo-box p-3 m-3">
-            <img src="image/logo.svg"></img>
+      <h2 className="college-name">Bipin Tripathi Kumaon Institute of Technology</h2>
+      <div className="header-container">
+        <div className="left-container">
+          <img src={logo} alt="Fest-logo" />
         </div>
-        <div className="content">
-            <pre>
-               Start Fest from 18 April to  20 April 2k22
-            </pre>
+        <div className="right-container">
+          <div className="clip"></div>
+          <div>
+            <h1 className="fest-h1">KAIVALYA</h1>
+            <h1 className="fest-h2">FEST</h1>
+            <h1 className="fest-h3">2K22</h1>
+          </div>
+          <p>Fest Start from 18 April to 19 April 2K22</p>
+
         </div>
 
       </div>
+
     </div>
   );
 }
