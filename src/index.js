d29d4bb6181e5ce8e50ef43b533f649387ae033c
Ayush Bisht <bisht.ayush2001@gmail.com>
2022-04-02 11:02:28 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import history from './components/utility/location'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+       <BrowserRouter history={history}   >
+         <Routes>
+         <Route exact path="/" element={ <App /> } />
+         </Routes>
+      </BrowserRouter>,
   document.getElementById('root')
 );
 
