c2c2b456631c481dd181ee045cede094ddc4e7dc
MohdAnas07 <anas98144@gmail.com>
2022-04-02 16:13:04 +0530

    update

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -10,36 +10,38 @@ export default function Body() {
 
     return (
         <>
-            <div class="container-fluid bg-dark ">
-                <div className="events">
-                    <h1 >Events</h1>
-                    <div class="row ">
-                        <div class="col-md-8 mx-auto">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="main-timeline2">
+            <div style={{ fontFamily: "Cabin, sans-serif" }} class="container-fluid bg-dark ">
+                <div class="row ">
+                    <div class="col-md-8 mx-auto">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="main-timeline2">
 
-                                        {
-                                            Timeline && Timeline.map(item => {
-                                                return (
-                                                    <>
-                                                        <a class="timeline" href={`#events-${item.id}`}>
-                                                            <span class="icon fa fa-globe"></span>
-                                                            <p class="timeline-content"  >
-                                                                <h3 class="title">{item.title}</h3>
-                                                                <p class="description"  >
-                                                                    starts from {item.date} {"  "} {item.time}
-                                                                </p>
+                                    {
+                                        Timeline && Timeline.map(item => {
+                                            return (
+
+                                                <>
+                                                    <div class="timeline">
+                                                        <span class="icon fa fa-globe"></span>
+                                                        <p class="timeline-content">
+                                                            <h3 class="title">{item.title}</h3>
+                                                            <p class="description">
+                                                                starts from {item.date} {"  "} {item.time}
                                                             </p>
-                                                        </a>
-                                                    </>
-                                                )
-                                            })
-                                        }
-                                    </div>
+                                                        </p>
+                                                    </div>
+
+
+                                                </>
+                                            )
+                                        })
+                                    }
                                 </div>
                             </div>
                         </div>
+
+
                     </div>
                 </div>
             </div>
